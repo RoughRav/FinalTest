1826b01869eed7766dfbdde644635ae335ff2740
Ravil Gilyazov <roughrav@gmail.com>
2024-05-15 18:44:48 +0500

    code added

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿// Задача: Написать программу, которая из имеющегося массива строк формирует новый массив из строк, 
+// длина которых меньше, либо равна 3 символам. Первоначальный массив можно ввести с клавиатуры, 
+// либо задать на старте выполнения алгоритма. При решении не рекомендуется пользоваться коллекциями, 
+// лучше обойтись исключительно массивами.
+// Примеры:
+// [“Hello”, “2”, “world”, “:-)”] → [“2”, “:-)”]
+// [“1234”, “1567”, “-2”, “computer science”] → [“-2”]
+// [“Russia”, “Denmark”, “Kazan”] → []
+System.Console.WriteLine("Введите строку из слов, разделенных пробелами:");
+        string input = Console.ReadLine(); 
+        string[] originalArray = input.Split(' '); 
+        string[] newArray = CreateArrayMaxThreeChars(originalArray); 
+
+        PrintArray(originalArray);
+        PrintArray(newArray);
+
+        void PrintArray(string[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                System.Console.Write(array[i] + " ");
+            }
+            System.Console.WriteLine();
+        }
+
+        string[] CreateArrayMaxThreeChars(string[] array)
+        {
+            string[] newArray = new string[originalArray.Length]; 
+            int newIndex = 0; 
+
+            
+            foreach (string str in originalArray) 
+            {
+                
+                if (str.Length <= 3) 
+                {
+                    newArray[newIndex] = str; 
+                    newIndex++; 
+                }
+            }
+            return newArray;
+        }
